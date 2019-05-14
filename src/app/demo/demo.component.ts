12508f538987c720ccf07d486e99abbe7bc2d73c
unknown <adritasharma.dev@gmail.com>
2019-05-15 00:30:38 +0530

    css modified

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -11,16 +11,16 @@ export class DemoComponent implements OnInit {
 
   employeeList: any = [
     { employeeId: 101, employeeName: "Adrita Sharma", doj: "01-02-2016", highestQualification: "Graduation" },
-    { employeeId: 102, employeeName: "Deepika Das", doj: "01-02-2016", highestQualification: "Post Graduation" },
-    { employeeId: 103, employeeName: "Ankita Sarkar", doj: "01-02-2016", highestQualification: "Diploma" },
-    { employeeId: 104, employeeName: "Drishani Dey", doj: "01-02-2016", highestQualification: "Senior Secondary" },
-    { employeeId: 105, employeeName: "Priya Bhatt", doj: "01-02-2016", highestQualification: "Matriculation" },
-    { employeeId: 106, employeeName: "Tamorita Singh", doj: "01-02-2016", highestQualification: "Graduation" },
-    { employeeId: 107, employeeName: "Pooja Mehra", doj: "01-02-2016", highestQualification: "Graduation" },
-    { employeeId: 108, employeeName: "Surbhi Kumari", doj: "01-02-2016", highestQualification: "Graduation" },
-    { employeeId: 109, employeeName: "Rai Paul", doj: "01-02-2016", highestQualification: "Post Graduation" },
-    { employeeId: 110, employeeName: "Ishika Mazumdar", doj: "01-02-2016", highestQualification: "Post Graduation" },
-    { employeeId: 111, employeeName: "Rinky Nath", doj: "01-02-2016", highestQualification: "Senior Secondary" },
+    { employeeId: 102, employeeName: "Deepika Das", doj: "21-07-2014", highestQualification: "Post Graduation" },
+    { employeeId: 103, employeeName: "Ankita Sarkar", doj: "14-12-2015", highestQualification: "Diploma" },
+    { employeeId: 104, employeeName: "Drishani Dey", doj: "31-05-2015", highestQualification: "Senior Secondary" },
+    { employeeId: 105, employeeName: "Priya Bhatt", doj: "20-08-2013", highestQualification: "Matriculation" },
+    { employeeId: 106, employeeName: "Tamorita Singh", doj: "16-02-2012", highestQualification: "Graduation" },
+    { employeeId: 107, employeeName: "Pooja Mehra", doj: "02-03-2010", highestQualification: "Graduation" },
+    { employeeId: 108, employeeName: "Surbhi Kumari", doj: "19-04-2012", highestQualification: "Graduation" },
+    { employeeId: 109, employeeName: "Rai Paul", doj: "25-10-2017", highestQualification: "Post Graduation" },
+    { employeeId: 110, employeeName: "Ishika Mazumdar", doj: "03-06-2018", highestQualification: "Post Graduation" },
+    { employeeId: 111, employeeName: "Rinky Nath", doj: "22-02-2019", highestQualification: "Senior Secondary" },
   ]
 
   employeeQualificationChart: any = {
